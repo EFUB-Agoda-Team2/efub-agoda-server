c44649dc19a48a49ba3626b843f6077829a9c1f7
gimye <gimyedev@gmail.com>
2025-05-17 19:22:55 +0900

    chore : initial setting

---
+++ src/main/java/efub/agoda_server/AgodaServerApplication.java
@@ -0,0 +1,13 @@
+package efub.agoda_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgodaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgodaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/efub/agoda_server/AgodaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package efub.agoda_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgodaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
