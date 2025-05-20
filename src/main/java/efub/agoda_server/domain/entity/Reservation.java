fd426b006b5c687217277316c736a1985c273ef8
yerin <gimyedev@gmail.com>
2025-05-20 19:32:32 +0900

    feat: 숙소, 객실, 예약, 숙소 이미지 엔티티 생성

---
+++ src/main/java/efub/agoda_server/domain/entity/Reservation.java
@@ -0,0 +1,59 @@
+package efub.agoda_server.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "reservation")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long resId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "st_id", nullable = false)
+    private Stay stay;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
+
+    @Column(name = "guest_first", nullable = false)
+    private String guestFirst;
+
+    @Column(name = "guest_last", nullable = false)
+    private String guestLast;
+
+    @Column(name = "guest_email", nullable = false)
+    private String guestEmail;
+
+    @Column(name = "guest_phone", nullable = false)
+    private String guestPhone;
+
+    @Column(name = "guest_request")
+    private String guestRequest;
+
+    @Column(name = "install_month", nullable = false)
+    private int installMonth;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "checkin_at", nullable = false)
+    private LocalDate checkinAt;
+
+    @Column(name = "checkout_at", nullable = false)
+    private LocalDate checkoutAt;
+}
---
+++ src/main/java/efub/agoda_server/domain/entity/StayImage.java
@@ -0,0 +1,24 @@
+package efub.agoda_server.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "stay_image")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class StayImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long stImgId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "st_id", nullable = false)
+    private Stay stay;
+
+    @Column(name = "st_image", nullable = false)
+    private String stImage;
+}
