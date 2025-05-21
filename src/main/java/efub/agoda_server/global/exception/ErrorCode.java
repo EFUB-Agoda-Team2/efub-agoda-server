e156decbbfc29e446459ad26dc72420f9bff8358
jettieb <bokjihee@naver.com>
2025-05-22 00:32:09 +0900

    feat: stay 리스트 조회 시 날짜 관련 예외처리

--- src/main/java/efub/agoda_server/global/exception/ErrorCode.java
+++ src/main/java/efub/agoda_server/global/exception/ErrorCode.java
@@ -7,9 +7,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
     // 타입(상태 코드, "메시지");
-
     // Default
-    ERROR(400, "요청 처리에 실패했습니다.");
+    ERROR(400, "요청 처리에 실패했습니다."),
+
+    //stay 관련 에러
+    PAST_CHECKIN_DATE(400, "체크인 날짜는 오늘 이후여야 합니다."),
+    INVALID_CHECKOUT_DATE(400, "체크아웃 날짜는 체크인 날짜보다 이후여야 합니다.");
 
     private final int status;
     private final String message;
--- src/main/java/efub/agoda_server/stay/service/StayService.java
+++ src/main/java/efub/agoda_server/stay/service/StayService.java
@@ -1,5 +1,7 @@
 package efub.agoda_server.stay.service;
 
+import efub.agoda_server.global.exception.CustomException;
+import efub.agoda_server.global.exception.ErrorCode;
 import efub.agoda_server.stay.domain.Stay;
 import efub.agoda_server.stay.dto.response.StayListResponse;
 import efub.agoda_server.stay.repository.StayRepository;
@@ -22,7 +24,11 @@ public class StayService {
 
     @Transactional(readOnly = true)
     public StayListResponse getStays(String city, int minPrice, int maxPrice, LocalDate checkIn, LocalDate checkOut, int page) {
-        Pageable pageable = PageRequest.of(page, 8);    //페이지당 데이터 수 8개 고정
+        if(checkIn.isBefore(LocalDate.now()))
+            throw new CustomException(ErrorCode.PAST_CHECKIN_DATE);
+        if(checkIn.isAfter(checkOut) || checkIn.isEqual(checkOut))
+            throw new CustomException(ErrorCode.INVALID_CHECKOUT_DATE);
+        Pageable pageable = PageRequest.of(page, 2);    //페이지당 데이터 수 8개 고정
         Page<Stay> stays = stayRepository.findBySalePriceBetweenAndCity(minPrice, maxPrice, city, pageable);
         return StayListResponse.from(city, checkIn, checkOut, stays);
     }
