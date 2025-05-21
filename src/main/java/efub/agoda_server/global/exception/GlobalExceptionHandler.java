8deacc1344a457f5dcbbed6ed5c34cecd71c8ec1
jettieb <bokjihee@naver.com>
2025-05-21 18:17:02 +0900

    feat: error handler 생성

---
+++ src/main/java/efub/agoda_server/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package efub.agoda_server.global.exception;
+
+import efub.agoda_server.global.exception.dto.ErrorDto;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    protected ResponseEntity<ErrorDto> handleCustomException(CustomException e, HttpServletRequest request) {
+        ErrorDto errorDto = new ErrorDto(
+                LocalDateTime.now().toString(),
+                e.getErrorCode().getStatus(),
+                e.getErrorCode().name(),
+                e.getErrorCode().getMessage(),
+                request.getRequestURI()
+        );
+        return new ResponseEntity<>(errorDto, HttpStatusCode.valueOf(e.getErrorCode().getStatus()));
+    }
+}
